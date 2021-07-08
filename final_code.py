835fd391182ac7b5d8dc603d8d2cca31b13de199
mritunjay7497 <mkumarmani97@gmail.com>
2021-07-08 18:58:53 +0530

    final code beta

--- final_code.py
+++ final_code.py
@@ -14,6 +14,29 @@ df = pd.read_csv("data_modified_1.csv")
 
 # Drop the duplicates entry and aggregate the dataframe rows in list
 non_dups = df.drop_duplicates().groupby(
-    ["Lab Number", "panel_code", 'Parameter Code']).agg(list)
+    ["Lab Number", "panel_code","panel_name","Parameter Code","Parameter Name"]).agg(list)
 
-print(non_dups)
+
+# save in an excel sheet
+non_dups.to_excel('output_test.xlsx')
+
+
+# load the saved excel sheet
+output_df = pd.read_excel('output_test.xlsx', sheet_name='Sheet1')
+
+df = df.fillna('NaN')
+
+def recur_dictify(frame):
+    if len(frame.columns) == 7:
+        if frame.values.size == 1: return frame.values[0][0]
+        return frame.to_dict()
+    grouped = frame.groupby(frame.columns[0])
+    d = {k: recur_dictify(g.iloc[:,1:]) for k,g in grouped}
+    return d
+
+required_format = recur_dictify(df.drop_duplicates())
+
+json.dumps(required_format,ensure_ascii=False)
+
+with open('myfile.json', 'w', encoding ='utf8') as json_file:
+    json.dump(required_format, json_file, ensure_ascii = False)
